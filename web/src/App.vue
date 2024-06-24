27f9a000d5fb88f0a4d6ea54ca1922ae404f7c16
Florian Loch <me@fdlo.ch>
2024-06-25 00:22:24 +0200

    feat: add 'for Spotify' to the header; improve slogans

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,22 +1,26 @@
 <template lang="pug">
 #app.v-cloak
-  .page-content
-    .header.p-3.center-sm
-      h1#title.only-mobile.align-bottom Cassette
-      img#logo.d-inline.mr-3(src="../assets/cassette_logo.svg")
-      h1#title.not-on-mobile.d-inline.align-bottom Cassette
-    keep-alive
-      router-view
-  .footer.p-3.text-center
-    p(v-if="$route.name === 'Main'")
-      a(href="/?showHelp=true") Show help
-    p
-      | Content provided by Spotify&reg;.
-      br
-      | Check out Cassette's source code at
-      |
-      a(href="https://github.com/florianloch/cassette") GitHub
-      | .
-    p
-      router-link(:to="{ name: 'Consent' }") Privacy policy / manage your data.
+    .page-content
+        .header.p-3.center-sm
+            h1#title.only-mobile.align-bottom Cassette
+                br
+                small for Spotify
+            img#logo.d-inline.mr-3(src="../assets/cassette_logo.svg")
+            h1#title.not-on-mobile.d-inline.align-bottom Cassette
+                | &nbsp;
+                small for Spotify
+        keep-alive
+            router-view
+    .footer.p-3.text-center
+        p(v-if="$route.name === 'Main'")
+            a(href="/?showHelp=true") Show help
+        p
+            | Content provided by Spotify&reg;.
+            br
+            | Check out Cassette's source code at
+            |
+            a(href="https://github.com/florianloch/cassette") GitHub
+            | .
+        p
+            router-link(:to="{ name: 'Consent' }") Privacy policy / manage your data.
 </template>
